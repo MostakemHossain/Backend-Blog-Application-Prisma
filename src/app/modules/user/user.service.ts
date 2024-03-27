4d7d9f864711ad30c43c6497388f91235c5316ab
MostakemHossain <mostakememon123@gmail.com>
2024-03-27 17:04:28 +0600

    exact field match

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -10,16 +10,24 @@ const createUser = async (payload: any) => {
 };
 
 const getAllUser = async (queryParams: any) => {
-  const { q } = queryParams;
+  const { q, ...otherQuery } = queryParams;
   const conditions = [];
   if (q) {
     conditions.push({
       OR: ["username", "email"].map((field) => ({
         [field]: {
           contains: q,
+          mode: "insensitive",
         },
       })),
     });
+  }
+
+  if (Object.keys(otherQuery).length > 0) {
+    const filterData = Object.keys(otherQuery).map((key) => ({
+      [key]: otherQuery[key],
+    }));
+    conditions.push(...filterData);
   }
   const result = await prisma.user.findMany({
     where: { AND: conditions },
